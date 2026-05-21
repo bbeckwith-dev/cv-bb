448fd2d9746da0e63c69a50fe3bedcd031356b36
B. Beckwith <33493824+learninglegion@users.noreply.github.com>
2026-05-21 18:57:08 -0400

    chore: remove @vercel/analytics in favor of GoatCounter
    Eliminates the script.js 404 error and unused Vercel analytics CSP entries.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode, lazy, Suspense, useState, useEffect, useRef, Component, type ReactNode, type ComponentType } from 'react'
 import { hydrateRoot, createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, useLocation, Link } from 'react-router-dom'
-import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 import GlobalNav from './GlobalNav.tsx'
@@ -174,7 +173,6 @@ const app = (
       </PageTransition>
       <GlobalChat />
       <GlobalMusic />
-      <Analytics />
     </BrowserRouter>
   </StrictMode>
 )
